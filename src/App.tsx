66c9dea2f439335d48a6bd973d41db344911ad31
Harrison Chapman <hchapma5@myune.edu.au>
2023-08-04 18:59:25 +1000

    set up file structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Header, UserProvider } from './components'
+import { Home, Login, SignUp, GameHistory, Game } from './pages'
+
+import style from './App.module.css'
 
 function App() {
-  return <div className='App'>Test</div>
+  return (
+    <>
+      <UserProvider>
+        <Header />
+        <main className={style.main}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='sign-up' element={<SignUp />} />
+            <Route path='game-history/:id' element={<GameHistory />} />
+            <Route path='game/:id' element={<Game />} />
+            <Route path='*' element={<Navigate to='/' replace />} />
+          </Routes>
+        </main>
+      </UserProvider>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import { useContext } from 'react'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+import { UserContext } from '../context'
+
+import style from './Header.module.css'
+
+export default function Header() {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { user, logout } = useContext(UserContext)
+
+  const getActions = () => {
+    if (user) {
+      return (
+        <>
+          <button
+            className={style.action}
+            onClick={() => navigate('game-history')}
+          >
+            Game History
+          </button>
+          <button
+            className={style.action}
+            onClick={() => {
+              logout()
+              navigate('/')
+            }}
+          >
+            Logout
+          </button>
+        </>
+      )
+    } else {
+      return location.pathname !== '/login' ? (
+        <button className={style.action} onClick={() => navigate('login')}>
+          Login
+        </button>
+      ) : (
+        <button className={style.action} onClick={() => navigate('sign-up')}>
+          Sign Up
+        </button>
+      )
+    }
+  }
+
+  return (
+    <header className={style.header}>
+      <div className={style.container}>
+        <Link to='/'>Gomoku</Link>
+        <div className={style.actions}>{getActions()}</div>
+      </div>
+    </header>
+  )
+}
