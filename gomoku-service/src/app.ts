4920d7c009589ea7d0efc34b34fb2fce6addc320
Harrison Chapman <hchapma5@myune.edu.au>
2023-09-23 22:04:52 +1000

    Installed cors on backend

--- gomoku-service/src/app.ts
+++ gomoku-service/src/app.ts
@@ -1,11 +1,22 @@
 import express, { Express } from 'express'
+import { createServer } from 'http'
+import cors from 'cors'
 import authHandler from './handler/auth.handler'
 import gameHandler from './handler/game.handler'
 
 const app: Express = express()
+
+app.use(
+  cors({
+    origin: process.env.allowHost || true,
+  })
+)
+
 app.use(express.json())
 
 app.use('/api/auth', authHandler)
 app.use('/api/game', gameHandler)
+
+export const server = createServer(app)
 
 export default app
